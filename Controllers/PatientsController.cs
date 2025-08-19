c29391247181bee6c2116c9af0a7c00e79d4ab9c
KiArAsHKaNaNi <kiarash.kanani2000@gmail.com>
2025-08-20 00:03:05 +0330

    Adding doctors Crud

--- Controllers/PatientsController.cs
+++ Controllers/PatientsController.cs
@@ -55,8 +55,7 @@ namespace A_Very_Simple_HIS.Controllers
         }
 
         // POST: Patients/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,DateOfBirth,GenderId,InsuranceId")] Patient patient)
@@ -91,8 +90,7 @@ namespace A_Very_Simple_HIS.Controllers
         }
 
         // POST: Patients/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,DateOfBirth,GenderId,InsuranceId")] Patient patient)
