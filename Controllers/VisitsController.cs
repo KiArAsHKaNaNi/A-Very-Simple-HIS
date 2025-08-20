2685d36e400054b717386b44767fab9766ce7e00
KiArAsHKaNaNi <kiarash.kanani2000@gmail.com>
2025-08-20 22:35:37 +0330

    Adding Visits controllers and views

---
+++ Controllers/VisitsController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using A_Very_Simple_HIS.Data;
+using A_Very_Simple_HIS.Models;
+
+namespace A_Very_Simple_HIS.Controllers
+{
+    public class VisitsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VisitsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Visits
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Visits.Include(v => v.Doctor).Include(v => v.Patient);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Visits/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var visit = await _context.Visits
+                .Include(v => v.Doctor)
+                .Include(v => v.Patient)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (visit == null)
+            {
+                return NotFound();
+            }
+
+            return View(visit);
+        }
+
+        // GET: Visits/Create
+        public IActionResult Create()
+        {
+            ViewData["DoctorId"] = new SelectList(_context.Doctors, "Id", "FullName");
+            ViewData["PatientId"] = new SelectList(_context.Patients, "Id", "FirstName");
+            return View();
+        }
+
+        // POST: Visits/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,PatientId,DoctorId,VisitDate,VisitType,Notes")] Visit visit)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(visit);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DoctorId"] = new SelectList(_context.Doctors, "Id", "Id", visit.DoctorId);
+            ViewData["PatientId"] = new SelectList(_context.Patients, "Id", "Id", visit.PatientId);
+            return View(visit);
+        }
+
+        // GET: Visits/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var visit = await _context.Visits.FindAsync(id);
+            if (visit == null)
+            {
+                return NotFound();
+            }
+            ViewData["DoctorId"] = new SelectList(_context.Doctors, "Id", "Id", visit.DoctorId);
+            ViewData["PatientId"] = new SelectList(_context.Patients, "Id", "Id", visit.PatientId);
+            return View(visit);
+        }
+
+        // POST: Visits/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,PatientId,DoctorId,VisitDate,VisitType,Notes")] Visit visit)
+        {
+            if (id != visit.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(visit);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VisitExists(visit.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DoctorId"] = new SelectList(_context.Doctors, "Id", "Id", visit.DoctorId);
+            ViewData["PatientId"] = new SelectList(_context.Patients, "Id", "Id", visit.PatientId);
+            return View(visit);
+        }
+
+        // GET: Visits/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var visit = await _context.Visits
+                .Include(v => v.Doctor)
+                .Include(v => v.Patient)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (visit == null)
+            {
+                return NotFound();
+            }
+
+            return View(visit);
+        }
+
+        // POST: Visits/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var visit = await _context.Visits.FindAsync(id);
+            if (visit != null)
+            {
+                _context.Visits.Remove(visit);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VisitExists(int id)
+        {
+            return _context.Visits.Any(e => e.Id == id);
+        }
+    }
+}
